75efc2c62c865650b37496f50fc44f627417d034
Filipe Guth <lfguth@gmail.com>
2013-02-11 01:34:26 -0200

    changing margins and positions

--- js/ui.js
+++ js/ui.js
@@ -3,12 +3,3 @@ $(function() {
   $(this).select();
 });
 })
-
-$(document).ready(function () {
-  function reorient(e) {
-    var portrait = (window.orientation % 180 == 0);
-    $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
-  }
-  window.onorientationchange = reorient;
-  window.setTimeout(reorient, 0);
-});
